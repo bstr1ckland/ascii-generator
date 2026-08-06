85754b5d69e02456a1c38198b2c4c38ec2c2fd46
bstr1ckland <ben.strickland@dal.ca>
2026-08-06 20:20:35 -0300

    Added image loading into main

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # img-2-ascii
 ASCII Image generator
+
+# Compiling and running
+`cargo run help`
+`cago run path`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use image::{ImageReader, ImageError};
+use indoc::indoc;
+use std::env;
+// use std::io::Cursor;
+use std::process;
+
+fn main() -> Result<(), ImageError> {
+    let args: Vec<String> = env::args().collect();
+    let help: &str = indoc! {"
+        Usage: $ cargo run -- [help] [path]
+        Options:
+            help , shows this help text
+            path , path to image to convert to ascii 
+    "};
+   
+    // First arg is target/debug/<project-name>, in this case it's target/debug/assci
+    if args.len() != 2 {
+        eprintln!("Error: Invalid amount of arguments. Run `cargo run help` for info");
+        process::exit(1);
+    }
+
+    if &args[1] == "help" {
+        println!("{help}");
+    }
+
+    let img_path: &String = &args[1];
+    let image = ImageReader::open(img_path)?.decode()?;
+
+    Ok(())
 }
