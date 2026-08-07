9d89ed541e9b4479f4dbe5f7e849bbd6bd1a7a5c
bstr1ckland <ben.strickland@dal.ca>
2026-08-07 16:58:20 -0300

    converted image to grayscale and extracted dimensions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use image::GenericImageView;
 use image::{ImageReader, ImageError};
 use indoc::indoc;
 use std::env;
@@ -13,7 +14,7 @@ fn main() -> Result<(), ImageError> {
             path , path to image to convert to ascii 
     "};
    
-    // First arg is target/debug/<project-name>, in this case it's target/debug/assci
+    // First arg is target/debug/<project-name> , with std::env .
     if args.len() != 2 {
         eprintln!("Error: Invalid amount of arguments. Run `cargo run help` for info");
         process::exit(1);
@@ -24,7 +25,19 @@ fn main() -> Result<(), ImageError> {
     }
 
     let img_path: &String = &args[1];
-    let image = ImageReader::open(img_path)?.decode()?;
+    let image: image::DynamicImage = ImageReader::open(img_path)?.decode()?.grayscale();
+    let brightness_codex: &str = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
+    
+    let dimensions = image.dimensions();
+    // need to downscale image first
+    
+
+    for pixel in image.pixels() {
+        // 1. find brightness level of the pixel
+        // 2. somehow convert that to a character in brightness_codex
+        // 3. add the character to a 2d string/character array
+        // 4. print the result C:
+    }
 
     Ok(())
 }
