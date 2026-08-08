089837e7d76827d704d4fe780f42659f33a96097
bstr1ckland <ben.strickland@dal.ca>
2026-08-08 10:54:37 -0300

    got it working yay!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use image::GenericImageView;
+use image::imageops;
 use image::{ImageReader, ImageError};
 use indoc::indoc;
+use ndarray::Array2;
 use std::env;
-// use std::io::Cursor;
 use std::process;
 
 fn main() -> Result<(), ImageError> {
     let args: Vec<String> = env::args().collect();
-    let help: &str = indoc! {"
+    let help = indoc! {"
         Usage: $ cargo run -- [help] [path]
         Options:
             help , shows this help text
@@ -24,19 +24,39 @@ fn main() -> Result<(), ImageError> {
         println!("{help}");
     }
 
-    let img_path: &String = &args[1];
-    let image: image::DynamicImage = ImageReader::open(img_path)?.decode()?.grayscale();
-    let brightness_codex: &str = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
-    
-    let dimensions = image.dimensions();
-    // need to downscale image first
-    
+    let img_path = &args[1];
+    // Store an convert image to grayscale
+    let image = ImageReader::open(img_path)?.decode()?.grayscale();
+    let mut grayscale_image = image::imageops::colorops::grayscale(&image);
 
-    for pixel in image.pixels() {
-        // 1. find brightness level of the pixel
-        // 2. somehow convert that to a character in brightness_codex
-        // 3. add the character to a 2d string/character array
-        // 4. print the result C:
+    // Scale height down, since one pixel = ~1/2 ascii character
+    let width = 50 as usize;
+    let height = 25 as usize;
+
+    grayscale_image = imageops::thumbnail(&grayscale_image, width as u32, height as u32);
+    
+    let mut ascii_image = Array2::<char>::from_elem((height, width), ' ');
+
+    // pixel is a Luma<u8>
+    for (x, y, image::Luma([gray_value])) in grayscale_image.enumerate_pixels() {
+        let ascii_char = match gray_value {
+            0..=24 => ' ',
+            25..=49 => '.',
+            50..=74 => ':',
+            75..=99 => '-',
+            100..=124 => '=',
+            125..=149 => '+',
+            150..=174 => '*',
+            175..=199 => '#',
+            200..=224 => '%',
+            _ => '@',
+        };
+        ascii_image[[y as usize, x as usize]] = ascii_char;
+    }
+
+    for row in ascii_image.rows() {
+        let line: String = row.iter().collect();
+        println!("{}", line);
     }
 
     Ok(())
